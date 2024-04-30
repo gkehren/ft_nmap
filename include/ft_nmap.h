6027f59ade9a912457a1baa1adf4f865fa78df49
gkehren <kehren.guillaume@gmail.com>
2024-04-30 20:56:10 +0200

    Add Makefile, ft_nmap.h, and ft_nmap.c files

---
+++ include/ft_nmap.h
@@ -0,0 +1,6 @@
+#ifndef FT_NMAP_H
+#define FT_NMAP_H
+
+#include <stdio.h>
+
+#endif
---
+++ src/ft_nmap.c
@@ -0,0 +1,9 @@
+#include "../include/ft_nmap.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	printf("ft_nmap\n");
+	return (0);
+}
